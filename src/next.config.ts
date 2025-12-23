235a7e255956614f1453aa1d5c76ed8fe9173613
amatsu4510 <tibawwww@gmail.com>
2025-12-24 00:33:21 +0900

    ブログ記事の画像最適化

--- src/next.config.ts
+++ src/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+      remotePatterns: [
+        // もし自分のドメイン (shoat-portfolio.com) でも配信しているなら、それも追加しておくと安全です
+        {
+          protocol: 'https',
+          hostname: 'shoat-portfolio.com',
+          pathname: '/**',
+        },
+      ],
+    },
 };
 
 export default nextConfig;
